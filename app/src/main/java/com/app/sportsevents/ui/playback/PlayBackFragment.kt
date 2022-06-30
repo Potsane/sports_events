5599030e427566574a13b03c6df6c20670b4059a
Potsane Mohale <potsane@luno.com>
2022-06-30 22:25:00 +0200

    Initial commit

---
+++ app/src/main/java/com/app/sportsevents/ui/playback/PlayBackFragment.kt
@@ -0,0 +1,42 @@
+package com.app.sportsevents.ui.playback
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.app.sportsevents.databinding.FragmentNotificationsBinding
+
+class PlayBackFragment : Fragment() {
+
+    private var _binding: FragmentNotificationsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val playBackViewModel =
+            ViewModelProvider(this).get(PlayBackViewModel::class.java)
+
+        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textNotifications
+        playBackViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
