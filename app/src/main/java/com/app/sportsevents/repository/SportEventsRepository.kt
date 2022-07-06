059965b0e47b4990800c27d9812e50a92ebe734d
potsane <potsanemohale@gmail.com>
2022-07-06 17:56:37 +0200

    Code clean up

--- app/src/main/java/com/app/sportsevents/repository/SportEventsRepository.kt
+++ app/src/main/java/com/app/sportsevents/repository/SportEventsRepository.kt
@@ -1,15 +1,10 @@
 package com.app.sportsevents.repository
 
 import com.app.sportsevents.network.SportsEventsApiService
-import com.app.sportsevents.network.entity.SportEvent
-import retrofit2.Response
 import javax.inject.Inject
 
 class SportEventsRepository @Inject constructor(private val apiService: SportsEventsApiService) {
 
     suspend fun getEvents() = apiService.getEvents()
-    suspend fun getSchedule(): Response<List<SportEvent>> {
-        println("------ call made")
-        return apiService.getSchedule()
-    }
+    suspend fun getSchedule() = apiService.getSchedule()
 }
