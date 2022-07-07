bc60ca4edc06ad47fd6ee3ff018e7ce33b1132b5
potsane <potsanemohale@gmail.com>
2022-07-07 14:46:30 +0200

    Add date

--- app/src/main/java/com/app/sportsevents/utils/DateTimeUtils.kt
+++ app/src/main/java/com/app/sportsevents/utils/DateTimeUtils.kt
@@ -1,29 +1,63 @@
 package com.app.sportsevents.utils
 
-import android.os.Build
-import androidx.annotation.RequiresApi
-import java.time.Instant
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.TimeZone
 
-private fun getTime(timeMillis: Long): String {
-    val minute = (timeMillis / (1000 * 60)) % 60
-    val hour = (timeMillis / (1000 * 60 * 60)) % 24
-    return "$hour:$minute"
+fun getDateObject(timeMillis: Long): DateObject {
+    val date = Date(timeMillis)
+    val calendar = Calendar.getInstance()
+    calendar.time = date
+
+    val dayOfTheMonth = calendar.get(Calendar.DAY_OF_MONTH)
+    val month = calendar.get(Calendar.MONTH)
+    val year = calendar.get(Calendar.YEAR)
+
+    return DateObject(
+        getDayOfWeek(timeMillis),
+        dayOfTheMonth,
+        getMonthOfYear(month),
+        year,
+        getTime(timeMillis)
+    )
 }
 
-private fun getDateDifference(timeMillis: Long): Long {
-    val difference = System.currentTimeMillis() - timeMillis
-    return (difference / (1000 * 60 * 60 * 24)) % 365
+data class DateObject(
+    var dayOfWeek: String,
+    var dayOfTheMonth: Int,
+    var month: String,
+    var year: Int,
+    var time: String
+)
+
+fun getDayOfWeek(timeMillis: Long): String {
+    val calendar = Calendar.getInstance()
+    calendar.time = Date(timeMillis)
+    val formatter = SimpleDateFormat("EEEE")
+    return formatter.format(calendar.time)
 }
 
-@RequiresApi(Build.VERSION_CODES.O)
-fun getFormattedDate(timeStamp: String): String {
-    val timeMillis = Instant.parse(timeStamp).toEpochMilli()
-    val dayDifference = getDateDifference(timeMillis)
-    return when {
-        dayDifference == 0L -> "Today - ${getTime(timeMillis)}"
-        dayDifference == 1L -> "Tomorrow - ${getTime(timeMillis)}"
-        dayDifference == -1L -> "Yesterday - ${getTime(timeMillis)}"
-        dayDifference < -1L -> "$dayDifference Days Ago"
-        else -> "In $dayDifference days"
+fun getTime(timeMillis: Long): String {
+    val dateFormat = SimpleDateFormat("HH:mm a")
+    dateFormat.timeZone = TimeZone.getTimeZone("UTC")
+    return dateFormat.format(Date(timeMillis))
+}
+
+fun getMonthOfYear(representation: Int): String {
+    return when (representation) {
+        1 -> "January"
+        2 -> "February"
+        3 -> "March"
+        4 -> "April"
+        5 -> "May"
+        6 -> "June"
+        7 -> "July"
+        8 -> "August"
+        9 -> "September"
+        10 -> "October"
+        11 -> "November"
+        12 -> "December"
+        else -> ""
     }
 }
